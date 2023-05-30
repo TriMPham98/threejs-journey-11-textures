1cde7ed4031d6f566b461e852c34100d31d9eb8b
Tri <trimpham98@gmail.com>
2023-05-30 00:04:02 -0700

    Use Three.js TextureLoader class

--- src/script.js
+++ src/script.js
@@ -4,14 +4,8 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 /**
  * Textures
  */
-const image = new Image();
-const texture = new THREE.Texture(image);
-
-image.onload = () => {
-  texture.needsUpdate = true;
-};
-
-image.src = "/textures/door/color.jpg";
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("/textures/door/color.jpg");
 
 /**
  * Base
