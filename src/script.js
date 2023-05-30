5a4b657e0b234a1df2263674b4ad724b8e35cd8f
Tri <trimpham98@gmail.com>
2023-05-30 00:17:49 -0700

    Load more images for textures

--- src/script.js
+++ src/script.js
@@ -23,18 +23,13 @@ loadingManager.onError = () => {
 };
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load(
-  "/textures/door/color.jpg",
-  () => {
-    console.log("load");
-  },
-  () => {
-    console.log("progress");
-  },
-  () => {
-    console.log("error");
-  }
-);
+const colorTexture = textureLoader.load("/textures/door/color.jpg");
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const ambientOcclusionTexture = textureLoader.load("/textures/door/ambientOcclusion.jpg");
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
 
 /**
  * Base
